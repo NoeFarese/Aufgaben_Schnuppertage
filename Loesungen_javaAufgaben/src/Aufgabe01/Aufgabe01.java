5bdf4a110d80706674b34e58bb0e27841e99fc04
p20668 <eric.hoesli@css.ch>
2025-04-07 09:46:36 +0200

    SEB-fuenfhebiger-jambus: change example for less confusion

--- Loesungen_javaAufgaben/src/Aufgabe01/Aufgabe01.java
+++ Loesungen_javaAufgaben/src/Aufgabe01/Aufgabe01.java
@@ -2,12 +2,17 @@ package Aufgabe01;
 
 public class Aufgabe01 {
     public static void main(String[] args){
-        //Erstelle eine Variabel mit dem Namen name in welche du deinen Namen schreibst.
-        String name = "mein Name";
-        //Erstelle eine Variabel mit dem Namen alter in welche du deinen alter speicherst.
-        int alter = 16;
-        //Gebe jetzt diesen satz aus:
-        //Mein Alter ist alter und mein Name ist name.
-        System.out.println("Mein alter ist " + alter + " und mein Name ist " + name);
+        // Erstelle eine Variabel mit dem Namen lieblingsEssen und speichere darin dein Lieblingsessen.
+
+        String lieblingsEssen = "Pizza";
+
+        // Erstelle eine Variabel mit dem Namen alter und speichere darin dein Alter.
+
+        int alter = 19;
+
+        // Gebe jetzt diesen Satz mithilfe der Variabeln aus:
+        // Mein Alter ist <alter> und mein Lieblingsessen ist <lieblingsEssen>.
+
+        System.out.println("Mein Alter ist " + alter + " und mein Lieblingsessen ist " + lieblingsEssen + ".");
     }
 }
--- Schnuppertag/src/ch/css/schnuppertag/aufgabe_1/Aufgabe.java
+++ Schnuppertag/src/ch/css/schnuppertag/aufgabe_1/Aufgabe.java
@@ -2,11 +2,18 @@ package aufgabe_1;
 
 public class Aufgabe {
     public static void main(String[] args){
-        // Erstelle eine Variabel, welche du name benennst und speichere deinen Namen darin.
+        // Erstelle eine Variabel mit dem Namen lieblingsEssen und speichere darin dein Lieblingsessen.
 
-        // Erstelle eine Variabel, welche du alter benennest und speichere dein Alter darin.
 
-        // Gebe jetzt diesen Satz mit Hilfe der Variabeln aus:
-        // Mein Alter ist <alter> und mein Name ist <name>.
+
+        // Erstelle eine Variabel mit dem Namen alter und speichere darin dein Alter.
+
+
+
+        // Gebe jetzt diesen Satz mithilfe der Variabeln aus:
+        // Mein Alter ist <alter> und mein Lieblingsessen ist <lieblingsEssen>.
+
+
+
     }
 }
