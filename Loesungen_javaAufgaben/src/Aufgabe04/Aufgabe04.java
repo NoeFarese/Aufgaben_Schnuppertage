7cbee484698c1ab5ab5fec40fa5d8fbd379409b8
p20668 <eric.hoesli@css.ch>
2025-04-08 13:27:39 +0200

    SDM-zweihebiger-trochaeus: change wording

--- Loesungen_javaAufgaben/src/Aufgabe04/Aufgabe04.java
+++ Loesungen_javaAufgaben/src/Aufgabe04/Aufgabe04.java
@@ -2,18 +2,19 @@ package Aufgabe04;
 
 public class Aufgabe04 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-        }
+        // Erstelle nun zwei Schleifen, welche die Zahlen von 1 bis 10 ausgeben.
+        // Verwende für die erste while und für die zweite for.
 
-        System.out.println();
-        System.out.println("__________________________________________-");
-        System.out.println();
-
-        int x = 0;
+        int x = 1;
         while (x <= 10) {
             System.out.println(x);
             x++;
+        }
+
+        System.out.println("\n____________________________________________\n");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 }
--- Schnuppertag/src/ch/css/schnuppertag/aufgabe_4/Aufgabe.java
+++ Schnuppertag/src/ch/css/schnuppertag/aufgabe_4/Aufgabe.java
@@ -3,7 +3,8 @@ package aufgabe_4;
 public class Aufgabe {
 
     public static void main(String[] args){
-        //Erstelle nun zwei Schleifen, welche bis auf 10 zählen. Mache dies mit beiden Variante.
+        // Erstelle nun zwei Schleifen, welche die Zahlen von 1 bis 10 ausgeben.
+        // Verwende für die erste while und für die zweite for.
 
 
     }
