eeb063d5fcd215987eb636acf8cb054b9c211e84
p20668 <eric.hoesli@css.ch>
2025-04-16 14:19:16 +0200

    SDM-dreihebiger-daktylus: fix typos

--- Schnuppertag/src/ch/css/schnuppertag/aufgabe_3/Aufgabe.java
+++ Schnuppertag/src/ch/css/schnuppertag/aufgabe_3/Aufgabe.java
@@ -4,12 +4,12 @@ public class Aufgabe {
 
     public static void main(String[] args) {
         // Nr. 1:
-        // Erstelle eine Variable names x und weise ihr den Wert 15 zu. Prüfe dann, ob die Variable 15 ist und fall ja, gib in der Konsole folgendes aus: Die Variable x ist 15.
+        // Erstelle eine Variable names x und weise ihr den Wert 15 zu. Prüfe dann, ob die Variable denn Wert 15 hat und falls ja, gib in der Konsole folgendes aus: Die Variable x ist 15.
 
 
         // Nr. 2:
-        // Mit der bereits erstellten Variable x kannst du jetzt eine Abfrage erstellen, welche überprüpft, ob die Variable x 10 ist, falls ja gib aus: Die Variable x ist 10.
-        // Falls es nicht zutreffen sollte, gib in der Konsole aus: Die Variable x ist nicht 10.
+        // Mit der bereits erstellten Variable x kannst du jetzt eine Abfrage erstellen, welche überprüft, ob die Variable x den wert 10 hat, falls ja, gib aus: Die Variable x ist 10.
+        // Falls die Variable einen anderen Wert als 10 hat, gib in der Konsole aus: Die Variable x ist nicht 10.
 
 
     }
